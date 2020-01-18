25a62be1daa657e106a3454a7fa59207864125e8
root <root@DESKTOP-VIB4Q10.localdomain>
2020-01-18 11:11:06 -0700

    updated data more

--- ws/ws/ws/adopterData.py
+++ ws/ws/ws/adopterData.py
@@ -12,20 +12,10 @@ age = 5
 @app.route("/adopterData")
 def adopterData(): 
     mongoDB.adopters.insert_one({
-        "AnimalType": "hackaztest",
-        "homeType":"hackaztest",
-        "homeStatus":"hackaztest",  
-        "planToMove": "",
-        "do-not-adopt": False,
-        "Previous Pets": "yes",
-        "sprayed": False,
-        "years owned": 5,
-        "vetInfo": {
-            'currentVet': 'yes',
-            'emergencyVet':'yes'
-        },
-        "criminal record": False,        
-        'lengthOfStay': 0,
+
+        "name": "justin",
+        "pass": "void",
+        "email": "jcanode@my.gcu.edu",
         "score": 0,
     })
     return render_template(
